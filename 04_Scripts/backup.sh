71e5994f53e66909adec6ce9a89ed8977301be72
Alex <abellan2725@gmail.com>
2026-03-10 12:37:31 +0100

    Refactor backup script for clarity and formatting

--- 04_Scripts/backup.sh
+++ 04_Scripts/backup.sh
@@ -8,21 +8,21 @@
 # ------------------------------------------------------------------
 
 # 1. Definición de Variables 
-FECHA=$(date +%Y-%m-%d_%H%M) [cite: 847, 861, 920]
-DESTINO="/srv/backups" [cite: 848, 862, 922]
-ORIGEN="/srv/climair/facturacion" [cite: 849, 863, 923]
+FECHA=$(date +%Y-%m-%d_%H%M)
+DESTINO="/srv/backups" 
+ORIGEN="/srv/climair/facturacion" 
 
-echo "Iniciando copia de seguridad de Climair..." [cite: 850, 864, 873]
+echo "Iniciando copia de seguridad de Climair..." 
 
 # 2. Creación del empaquetado comprimido (tar -czf) 
 # c: crear, z: comprimir (gzip), f: nombre del archivo 
-tar -czf $DESTINO/backup_facturas_$FECHA.tar.gz $ORIGEN [cite: 853, 865]
+tar -czf $DESTINO/backup_facturas_$FECHA.tar.gz $ORIGEN 
 
 # 3. Mantenimiento y Rotación (Borrado de copias > 7 días) 
 # Evita que el volumen lógico LVM se llene por acumulación 
-find $DESTINO -type f -mtime +7 -name "*.tar.gz" -delete [cite: 854, 865, 929, 930]
+find $DESTINO -type f -mtime +7 -name "*.tar.gz" -delete 
 
-echo "Backup finalizado con éxito en $DESTINO" [cite: 855, 866, 874]
+echo "Backup finalizado con éxito en $DESTINO" 
 
 #NOTAS!! 
 #Permisos: Es fundamental otorgar permisos de ejecución con sudo chmod +x /usr/local/bin/backup_climair.sh.
